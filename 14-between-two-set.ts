e474efef717dfb36fe8561b188bc7bbfedb1a52e
S4v!0r <109929385+S4vi0r17@users.noreply.github.com>
2024-03-22 23:03:37 -0500

    Refactor getTotalX function to use array.every method

--- 14-between-two-set.ts
+++ 14-between-two-set.ts
@@ -16,6 +16,33 @@
 */
 
 ; (() => {
+    // function getTotalX(a: number[], b: number[]): number {
+
+    //     let result: number = 0
+
+    //     let a_min: number = Math.min(...a)
+    //     let b_max: number = Math.max(...b)
+
+    //     for (let i = a_min; i <= b_max; i++) {
+    //         let flag: boolean = true
+    //         for (let j = 0; j < a.length; j++) {
+    //             if (!(i % a[j] === 0)) {
+    //                 flag = false
+    //             }
+    //         }
+    //         for (let j = 0; j < b.length; j++) {
+    //             if (!(b[j] % i === 0)) {
+    //                 flag = false
+    //             }
+    //         }
+    //         if (flag) {
+    //             result++
+    //         }
+    //     }
+
+    //     return result
+    // }
+
     function getTotalX(a: number[], b: number[]): number {
 
         let result: number = 0
@@ -24,18 +51,8 @@
         let b_max: number = Math.max(...b)
 
         for (let i = a_min; i <= b_max; i++) {
-            let flag: boolean = true
-            for (let j = 0; j < a.length; j++) {
-                if (!(i % a[j] === 0)) {
-                    flag = false
-                }
-            }
-            for (let j = 0; j < b.length; j++) {
-                if (!(b[j] % i === 0)) {
-                    flag = false
-                }
-            }
-            if (flag) {
+
+            if ((a.every(n => i % n === 0)) && b.every((n) => n % i === 0)) {
                 result++
             }
         }
@@ -43,7 +60,7 @@
         return result
     }
 
-    const arr: number[] = [2, 3, 6] 
+    const arr: number[] = [2, 3, 6]
     const brr: number[] = [42, 84]
 
     const total = getTotalX(arr, brr);
