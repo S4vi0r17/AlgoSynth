35a2f865c3b8c0bc8020e0b1b4ded7d9e4f48350
S4vi0r <gbp17@outlook.com>
2024-03-28 17:44:38 -0500

    18-migratory-birds.ts

---
+++ 18-migratory-birds.ts
@@ -0,0 +1,55 @@
+/*
+    Given an array of bird sightings where every element represents a bird type id, determine the id of the most frequently sighted type. If more than 1 type has been spotted that maximum amount, return the smallest of their ids.
+
+    Example
+
+    `arr = [1, 1, 2, 2, 3]`
+
+    There are two each of types `1` and `2`, and one sighting of type `3`. Pick the lower of the two types seen twice: type `1`. 
+*/
+
+; (() => {
+    function migratoryBirds(arr: number[]): number {
+        let result: number[] = []
+        const set = [...new Set(arr)]
+        let map = new Map<number, number>();
+
+        console.log(map);
+
+        set.forEach((element) => {
+            map.set(element, 0);
+        });
+
+        arr.forEach((ar) => {
+            let count = map.get(ar)
+            map.set(ar, count! + 1)
+        });
+
+        let firstEntry = Array.from(map.entries())[0];
+        let maxValue = firstEntry[1];
+
+        map.forEach((value, key) => {
+            if (value >= maxValue) {
+                maxValue = value
+            }
+        });
+
+        // console.log(map.entries());
+        // console.log(map.keys());
+
+        for (const [key, value] of map.entries()) {
+            // console.log(`Key: ${key}, Value: ${value}`);
+            if (value === maxValue) {
+                result.push(key)
+            }
+        }
+
+        return Math.min(...result)
+    }
+
+    const arr: number[] = [1, 1, 2, 2, 3]
+    // const arr: number[] = [1, 4, 4, 4, 5, 3]
+    const result: number = migratoryBirds(arr);
+    console.log(result);
+
+})()
