5f2bd40a4fbbe444888894422781afdb186a5474
S4v!0r <gbp17@outlook.com>
2025-01-05 23:28:52 -0500

    Remove package.json and package-lock.json; wrap solveMeFirst function in an IIFE

--- 01.ts
+++ 01.ts
@@ -1,11 +1,13 @@
-function solveMeFirst(a: number, b: number): number {
-	if (1 <= a && a <= 1000 && 1 <= b && b <= 1000) {
-		return a + b
-	}
-	return -1
-}
+(() => {
+  function solveMeFirst(a: number, b: number): number {
+    if (1 <= a && a <= 1000 && 1 <= b && b <= 1000) {
+      return a + b;
+    }
+    return -1;
+  }
 
-let a = 2
-let b = 3
-let res = solveMeFirst(a, b)
-console.log(res)
+  let a = 2;
+  let b = 3;
+  let res = solveMeFirst(a, b);
+  console.log(res);
+})();
